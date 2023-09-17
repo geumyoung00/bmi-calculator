5945324c208f93bdfea98b0d0b62148ae0f9fe3d
geumyoung <1gyn093@gmail.com>
2023-09-17 22:23:27 +0900

    inputHandler와 value 생성

--- src/App.js
+++ src/App.js
@@ -1,22 +1,49 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-	return (
-		<main>
-			<h1>Project 4: BMI CALCULATOR</h1>
-			<div className="input-section">
-				<p class="slider-output">Weight: 50 kg</p>
-				<input className="input-slider" />
-				<p class="slider-output">Height: 160 cm</p>
-				<input className="input-slider" />
-			</div>
-			<div className="output-section">
-				<p>Your BMI is</p>
-				<p className="output">{20}</p>
-			</div>
-		</main>
-	);
+  const [weight, setWeight] = useState(50);
+  const [height, setHeight] = useState(160);
+  const weightHandler = (e) => {
+    setWeight(e.target.value);
+  };
+  const heightHandler = (e) => {
+    setHeight(e.target.value);
+  };
+
+  return (
+    <main>
+      <h1>Project 4: BMI CALCULATOR</h1>
+      <div className="input-section">
+        {/* <p className="slider-output">Weight: 50 kg</p> */}
+        <p className="slider-output">{`Weight: ${weight} kg`}</p>
+        <input
+          className="input-slider"
+          type="range"
+          min="40"
+          max="220"
+          step="1"
+          defaultValue={weight}
+          onChange={weightHandler}
+        />
+        {/* <p className="slider-output">Height: 160 cm</p> */}
+        <p className="slider-output">{`Height: ${height} cm`}</p>
+        <input
+          className="input-slider"
+          type="range"
+          min="140"
+          max="220"
+          step="1"
+          defaultValue={height}
+          onChange={heightHandler}
+        />
+      </div>
+      <div className="output-section">
+        <p>Your BMI is</p>
+        <p className="output">{20}</p>
+      </div>
+    </main>
+  );
 }
 
 export default App;
